30b650eb28f47adc6743e3cc7d82360aec2ed646
Sahil Karkhanis <karksahi@amazon.com>
2025-03-12 17:32:20 +0000

    Revert "Remove auth"
    This reverts commit 9df57f05a781cc30bdf1d6d2aa36c0c43b73abe5.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import { Amplify } from "aws-amplify";
 import awsExports from "../amplify_outputs.json";
 Amplify.configure(awsExports);
+import { Authenticator } from "@aws-amplify/ui-react";
 import "./index.css";
 import "@aws-amplify/ui-react/styles.css";
 import { BrowserRouter } from "react-router-dom";
@@ -11,7 +12,9 @@ import { BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <Authenticator>
         <App />
+      </Authenticator>
     </BrowserRouter>
   </StrictMode>
 );
